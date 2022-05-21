c3b8e8e50cf4d73133198fdb677594980a0aff09
OkochaEH <oussamaayachefff@gmail.com>
2022-05-21 22:18:11 +0100

    create the script which make the project run make sure it's all done

---
+++ script.js
@@ -0,0 +1,41 @@
+let progress = document.getElementById("prog")
+let prev = document.getElementById("prev")
+let next = document.getElementById("next")
+let circles = document.querySelectorAll(".circle")
+
+let crrActive = 1 
+
+next.addEventListener("click", _ => {
+  crrActive++
+  if (crrActive > circles.length) {
+    crrActive = circles.length
+  }
+  update()
+})
+prev.addEventListener("click", _ => {
+  crrActive--
+  if (crrActive < 1) {
+    crrActive = 1
+  }
+  update()
+})
+function update() {
+  circles.forEach( (circle, index) => {
+    if (index < crrActive) {
+      circle.classList.add("active")
+    }else {
+      circle.classList.remove("active")
+    }
+    let actives = document.querySelectorAll(".active")
+    progress.style.width = ((actives.length -1 ) / (circles.length - 1)) * 100 + "%" 
+    if (crrActive === 1) {
+      prev.disabled = true
+    } else if (crrActive === circles.length) {
+      next.disabled = true
+    }else{
+      prev.disabled = false
+      next.disabled = false
+      
+    }
+  })
+}
